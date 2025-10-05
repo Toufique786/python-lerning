dadea8edcde5d964766b573e83635286fd10ee6f
toufique789 <toufiquealambusiness@gmail.com>
2025-10-05 23:04:50 +0530

    first commit

---
+++ operation.py
@@ -0,0 +1,5 @@
+# Perform Basic Mathematical Operations
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Addition:",a+b,"\nSubstraction:",a-b,"\nMultiplication:",a*b,"\nDivision:",a/b)
+
