9a434e73d09774036d913fb967307f4c1290913a
toufique789 <toufiquealambusiness@gmail.com>
2025-10-12 20:57:21 +0530

    Assignment 3

---
+++ Assignment2/function.py
@@ -0,0 +1,32 @@
+# def grade_checker():
+#     user_input = int(input("enter your marks to check grade 0-100:  "))
+#     if user_input >=99:
+#             print("You obtaine grade 'A'")
+#     elif user_input >=75:
+#         print("You obtaine grade'B'")
+#     elif user_input >=55:
+#         print("You obtaine grade'C'")
+#     elif user_input >=33:
+#         print("You obtaine grade'D'")
+#     elif user_input <33:
+#         print("You obtaine grade'F'")
+#     else:
+#         print('No data match')
+
+#     print("Thanks for using grade checker")
+
+# grade_checker()
+
+# def num (a,b):
+#        c = a*b
+#        print(c)
+#        return c
+
+# num(55,33)
+
+
+# n = int(input("Enter your number :-  "))
+# total_sum = 0
+# for i in range(1,n+1):
+#     total_sum += i
+# print(total_sum)
